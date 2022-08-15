d318db26d9ceb163a67ec2ca445bf8aacbbf9278
Jean Meira <jean.meira10@hotmail.com>
2022-08-15 20:41:51 -0300

    feat: add timeToSeconds

--- src/Components/Stopwatch/index.tsx
+++ src/Components/Stopwatch/index.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import Button from "../Button";
 import Watch from "./Watch";
 import style from "./stopwatch.module.scss";
+import { timeToSeconds } from "../../common/utils/date";
 
 const Stopwatch: React.FC = () => {
+  console.log("conv: ", timeToSeconds("01:01:01"));
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}> Escolha um card e inicie o cronômetro </p>
